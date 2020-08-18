144a2f56984de246f07a0ba74beeeba0b6e169b6
Sean Wong <wotzhs@gmail.com>
2020-08-18 09:57:44 +0800

    master: add problem statement

--- 20200817_find_duplicates.cpp
+++ 20200817_find_duplicates.cpp
@@ -2,6 +2,23 @@
 #include <vector>
 using namespace std;
 
+/*
+
+This problem was recently asked by Amazon:
+
+Given an array of size n, and all values in the array are in the range 1 to n, find all the duplicates.
+
+class Solution(object):
+  def findDuplicates(self, nums):
+      # Fill this in.
+
+print(Solution().findDuplicates([4,3,2,7,8,2,3,1]))
+# [2, 3]
+
+*/
+
+// TC - o(n)
+// SC - o(n)
 vector<int> findDuplicates(vector<int> nums) {
 	vector<int> freq(nums.size(), 0), ans;
 	for (int i: nums) if (++freq[i]>1) ans.emplace_back(i);
